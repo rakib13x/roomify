e62bbdb9bb00789c2f3d86e53c1ce4439f860e85
rakib13x <rakib13x@gmail.com>
2026-02-21 16:36:14 +0600

    design item type added

---
+++ type.d.ts
@@ -0,0 +1,28 @@
+interface AuthState {
+  isSignedIn: boolean;
+  userName: string | null;
+  userId: string | null;
+}
+
+interface Material {
+  id: string;
+  name: string;
+  thumbnail: string;
+  type: "color" | "texture";
+  category: "floor" | "wall" | "furniture";
+}
+
+interface DesignItem {
+  id: string;
+  name?: string | null;
+  sourceImage: string;
+  sourcePath?: string | null;
+  renderedImage?: string | null;
+  renderedPath?: string | null;
+  publicPath?: string | null;
+  timestamp: number;
+  ownerId?: string | null;
+  sharedBy?: string | null;
+  sharedAt?: string | null;
+  isPublic?: boolean;
+}
